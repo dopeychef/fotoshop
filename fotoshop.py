d84c67f5575a4f32a94b861a7777b2b026e4490f
dopeychef <10640715@lammersvilleusd.net>
2017-05-25 17:48:25 -0700

    Added save feature

--- fotoshop.py
+++ fotoshop.py
@@ -15,13 +15,25 @@ class Fotoshop:
     def __init__(self):
         self.cwd = os.getcwd()
         self.deformer = Deformer()
+        self.outFolder = self.cwd + '/Out'
 
 
     ''' write the text passed over the image'''
-    def writeWatermark(self,im,text2Write):
+    def writeTextOver(self,im,text2Write,color='BLACK'):
+
+        colr = (0, 0, 0) #default text color is black
+        
+        if(color == 'WHITE'):
+            colr = (255, 255, 255)
+        elif(color == 'RED'):
+            colr = (255, 0, 0)
+        elif(color == 'BLUE'):
+            colr = (0, 0, 255)
+        elif(color == 'GREEN'):
+            colr = (0, 255, 0)
 
         #load the font from the fonts folder
-        ft = ImageFont.load(self.cwd + "/pilfonts/helvB24.pil")
+        ft = ImageFont.load(self.cwd + "/pilfonts/helvB14.pil")
 
         #get the size of the text to write
         wh = ft.getsize(text2Write)
@@ -30,10 +42,11 @@ class Fotoshop:
         # draw.line((0, im.size[1], im.size[0], 0), fill=(255, 255, 255))
         wh = ft.getsize(text2Write)
         # draw.text((im.size[0]/2 - wh[0]/2, im.size[1]/2 + 20), text2Write,fill=(0, 0, 0), font=ft)
-        draw.text((40, im.size[1]-60), text2Write,fill=(0, 0, 0), font=ft)
+        draw.text((40, im.size[1]-60), text2Write,fill=colr, font=ft)
         del draw  
         return im
 
+    '''overlay the image over the actual image'''
     def overlayImage(self,image,overlay):
         sizeImage = image.size
         #print(sizeImage)
@@ -45,20 +58,35 @@ class Fotoshop:
         image.paste(overlay,box,overlay)
         return image
 
-    #make it black and white
+    #make yje image black and white
     def makeBlackAndWhite(self,image):
         return image.convert('L')
 
-    # image filters
+    # image filters for adding special effcets
     def addSpecialEffects(self,image):
         #image = image.filter(ImageFilter.BLUR)
         #image = image.filter(ImageFilter.FIND_EDGES)
         image = image.filter(ImageFilter.EMBOSS)
         return image
 
+    '''deforms th eimage into a new one'''
     def deformImage(self,image):
         return ImageOps.deform(image, self.deformer)
 
-    def addFrame(self,image):
-        return ImageOps.expand(image,border=15,fill='red')
+    '''adds a frame of given color around the image'''
+    def addFrame(self,image,color,widthOfBorder):
+        return ImageOps.expand(image,border=widthOfBorder,fill=color)
+
+    '''save the image as the file name givem'''
+    def saveImage(self,image,fileName):
+        image.save(self.outFolder+ '/' + fileName +'.jpg')
+
+    '''displayes the image on screen'''
+    def showImage(self,image):
+        image.show()
+    
+    '''display and save the image'''
+    def showAndSave(self, image,fileName):
+        image.save(self.outFolder+  '/' + fileName + '.jpg')
+        image.show()
 
--- test-fotoshop.py
+++ test-fotoshop.py
@@ -4,41 +4,50 @@ from fotoshop import Fotoshop
 
 
 def testWriteWatermark(image):
-    image = fotoshop.writeWatermark(image,"fotoshop")
-    # display image on the screen
-    image.show()
+    image = fotoshop.writeTextOver(image,"fotoshop")
+    # display image on the screen and save to out folder
+    fotoshop.showAndSave(image, 'fsWaterMark')
+
 
 def testMakeBlackAndWhite(image):
     image = fotoshop.makeBlackAndWhite(image)
-    # display image on the screen
-    image.show()
+    # display image on the screen and save to out folder
+    fotoshop.showAndSave(image, 'fsBlacNwhite')
+
 
 def testAddSpecialEffects(image):
     image = fotoshop.addSpecialEffects(image)
-    # display image on the screen
-    image.show()
+    # display image on the screen and save to out folder
+    fotoshop.showAndSave(image, 'fsSpecialEffects')
+
 
 def testOverlayImage(image,overlay):
     image = fotoshop.overlayImage(image,overlay)
-    # display image on the screen
-    image.show()
+    # display image on the screen and save to out folder
+    fotoshop.showAndSave(image, 'fsOverlay')
     del image
 
 def testDeformImage(image):
     image = fotoshop.deformImage(image)
-    # display image on the screen
-    image.show()
+    # display image on the screen and save to out folder
+    fotoshop.showAndSave(image, 'fsDeform')
 
 def testAddFrame(image):
-    image = fotoshop.addFrame(image)
+    image = fotoshop.addFrame(image,'blue',15)
     # display image on the screen
-    image.show()
+    fotoshop.showAndSave(image, 'fsFrame')
 
 def testFrameOverlayAndWaterMark(image,overlay,textWatermark='fotoshop'):
-    image = fotoshop.addFrame(image)
+    image = fotoshop.addFrame(image,'red',25)
     image = fotoshop.overlayImage(image,overlay)
-    image = fotoshop.writeWatermark(image,textWatermark)
-    image.show()
+    image = fotoshop.writeTextOver(image,textWatermark)
+    fotoshop.showAndSave(image, 'fsFrameWatermarkNoverlay')
+
+def testAddSpecialEffectsWithOverLayAndText(image,overlay,overlaytext):
+    image = fotoshop.addSpecialEffects(image)
+    image = fotoshop.writeTextOver(image,overlaytext,'WHITE')
+    image = fotoshop.overlayImage(image,overlay)
+    fotoshop.showAndSave(image,'fsEffectsCombo')
 
 ''' main test method that calls various test method '''
 def runTestSuite():
@@ -49,7 +58,8 @@ def runTestSuite():
     overlay = Image.open(cwd + "/Images/mac-logo-transparent.png")
 
     #call the tests one by one
-    testFrameOverlayAndWaterMark(image, overlay, "fotoshop magic")
+    testFrameOverlayAndWaterMark(image, overlay, 'fotoshop magic')
+    testAddSpecialEffectsWithOverLayAndText(image,overlay,'fotoshop magic')
     testAddFrame(image)
     testAddSpecialEffects(image)
     testDeformImage(image)
@@ -60,6 +70,7 @@ def runTestSuite():
 
 # get current working directory
 cwd = os.getcwd()
+
 #initialize the Fotoshop module
 fotoshop = Fotoshop()
 
